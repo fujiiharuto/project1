aab6b22ca257213f738927a25b16ef36be22251c
fujiiharuto <clive.fujii@gmail.com>
2022-05-02 20:55:30 +0900

    project1

--- src/components/Datatable.js
+++ src/components/Datatable.js
@@ -1,65 +1,76 @@
-import React from "react";
+import * as React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from '@material-ui/core/styles';
 
 const columns = [
-  { field: "id", headerName: "ID", width: 70 },
-  { field: "firstName", headerName: "First name", width: 130 },
-  { field: "lastName", headerName: "Last name", width: 130 },
-  {
-    field: "age",
-    headerName: "Age",
-    type: "number",
-    width: 90
+  { 
+    field: 'id', 
+    headerName: 'ID', 
+    headerClassName: 'super-app-theme--header',
+    width: 70 
+  },
+  { 
+    field: 'firstName', 
+    headerName: 'First name', 
+    headerClassName: 'super-app-theme--header',
+    width: 130 
+  },
+  { 
+    field: 'lastName', 
+    headerName: 'Last name', 
+    headerClassName: 'super-app-theme--header',
+    width: 130 
   },
   {
-    field: "fullName",
-    headerName: "Full name",
-    description: "This column has a value getter and is not sortable.",
+    field: 'age',
+    headerName: 'Age',
+    headerClassName: 'super-app-theme--header',
+    type: 'number',
+    width: 90,
+  },
+  {
+    field: 'fullName',
+    headerName: 'Full name',
+    headerClassName: 'super-app-theme--header',
+    description: 'This column has a value getter and is not sortable.',
     sortable: false,
     width: 160,
     valueGetter: (params) =>
-      `${params.getValue("firstName") || ""} ${
-        params.getValue("lastName") || ""
-      }`
-  }
+      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+  },
 ];
 
 const rows = [
-  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 }
+  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
 ];
 
 const useStyles = makeStyles({
-  dataGrid: {
-    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
-    borderRadius: 3,
-    border: 0,
-    color: "white",
-    height: 48,
-    padding: "0 30px",
-    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
-    width: "640px"
-  }
+  root: {
+    '& .super-app-theme--header': {
+      backgroundColor: '#0a786c',
+    },
+  },
 });
 
-
-export default function Demo() {
+export default function DataTable() {
   const classes = useStyles();
   return (
-    <DataGrid
-      autoHeight={true}
-      disableExtendRowFullWidth={true}
-      columns={columns}
-      rows={rows}
-      className={classes.dataGrid}
-    />
+    <div style={{ height: 400, width: '100%' }} className={classes.root}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+        checkboxSelection
+      />
+    </div>
   );
 }
