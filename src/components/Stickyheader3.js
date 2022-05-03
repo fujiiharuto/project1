08e53ba0362192fb35558dcb0787ea99a6b04737
fujiiharuto <clive.fujii@gmail.com>
2022-05-04 00:01:06 +0900

    project1

--- src/components/Stickyheader3.js
+++ src/components/Stickyheader3.js
@@ -72,7 +72,7 @@ export default function StickyHeadTable() {
 
   return (
     <Paper sx={{ width: '100%', overflow: 'hidden' }}>
-      <TableContainer sx={{ maxHeight: 440 }} style={{ backgroundColor: "#10ff84" }}>
+      <TableContainer sx={{ maxHeight: 440 }} style={{ backgroundColor: "#e2f0d8" }}>
         <Table stickyHeader aria-label="sticky table">
           <TableHead>
             <TableRow>
